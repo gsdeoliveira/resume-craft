a1b27a02b015b75ea4ee38d49360a6cccea51907
gsdeoliveira <gabriielsousa2002@gmail.com>
2025-02-08 10:02:35 -0300

    feat: create aside navbar with toggle theme

---
+++ src/components/pages/dashboard/user-dropdown.tsx
@@ -0,0 +1,43 @@
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import Link from "next/link"
+import { LogOut, SquareUser } from "lucide-react"
+
+export const UserDropdown = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="w-full gap-2 justify-start px-2">
+          <Avatar className="w-7 h-7 block">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>GS</AvatarFallback>
+          </Avatar>
+          <p>Gabriel Sousa</p>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        align="center"
+        className="w-[var(--radix-dropdown-menu-trigger-width)]"
+      >
+        <Link passHref href="/dashboard/account">
+          <DropdownMenuItem className="gap-2">
+            <SquareUser size={16} />
+            Configurações de Conta
+          </DropdownMenuItem>
+        </Link>
+        <DropdownMenuItem className="text-red-500">
+          <LogOut size={16} />
+          Sair
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
